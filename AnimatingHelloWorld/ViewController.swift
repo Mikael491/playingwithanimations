c82dca6690b94f1ab2f4a5842e55b55ea652cb4f
Mikael <miketekle14@gmail.com>
2016-10-18 17:54:39 -0700

    connected ib outlets to story board objects and added code to initiate starting animations using UIView#animate

--- AnimatingHelloWorld/ViewController.swift
+++ AnimatingHelloWorld/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var animationLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        helloWorldLabel.center.y -= view.bounds.height
+        animationLabel.center.y += view.bounds.height
+        nameLabel.alpha = 0.0
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 1.0, animations: { 
+            self.helloWorldLabel.center.y += self.view.bounds.height
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 1.0, delay: 0.5, options: [], animations: { 
+            self.animationLabel.center.y -= self.view.bounds.height
+            }, completion: { finished -> Void in
+                UIView.animate(withDuration: 1.0, animations: { 
+                    self.nameLabel.alpha = 1.0
+                    }, completion: nil)
+        })
+        
+    }
+    
+    func backgroundColor() {
+        
+        UIView.animate(withDuration: 1.0, animations: { 
+            
+            }, completion: nil)
+        
+    }
 
 }
 
